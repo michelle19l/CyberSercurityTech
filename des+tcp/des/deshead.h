555ce3929f3939ea889718945ab3e0a2347ec15c
michelle19l <2675625405@qq.com>
2021-03-16 00:11:59 +0800

    1

---
+++ des+tcp/des/deshead.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <iostream>
+#include<fstream>
+#include<iomanip>
+#include<bitset>
+using namespace std;
+
+class tuple_
+{
+public:
+	int u_char_;//第几个字节，0开始
+	int bit_;//第几位，从左至右，0开始
+	tuple_() {};
+	tuple_(int a, int b) {
+		u_char_ = a;
+		bit_ = b;
+	}
+};
+//将字节索引该为字索引
+int u_char2bit(tuple_ a);
+//将字索引改为字节索引
+tuple_ bit2u_char(int a);
+
+//取第几位
+int getbit(u_char a, int x);//从左至右第0-7位
+//在第几位写入
+void write(u_char& a, int x, int num);
+//做对应转换
+void convert(u_char a[], u_char b[], int i, int conv[]);
+
+//初始置换
+void convIP(u_char a[], u_char b[]);//将a进行IP置换，结果存入b中
+//初始置换逆置换IP^(-1)
+void convIP_1(u_char a[], u_char b[]);//逆初始置换，将a进行IP置换，结果存入b中
+//密钥生成
+void getkeys(u_char key[8], u_char key_final[16][6]);
+//扩展置换
+void convertE(u_char a[4], u_char b[6]);
+//48bit异或，结果存到c中
+void xor_(u_char a[6], u_char b[6], u_char c[6]);
+
+//S盒
+void convertS(u_char a[6], u_char b[4]);
+void convertP(u_char a[4], u_char b[4]);
+
+//轮加密
+void round(u_char m[8], u_char key_final[16][6], u_char afterIP_1[8]);
+
---
+++ des+tcp/des/funcDef.cpp
@@ -0,0 +1,266 @@
+﻿//DES的加密和解密
+//加密和解密只是密钥的使用顺序相反，其余相同
+#include"deshead.h"
+#include"data.h"
+#include<stdio.h>
+//将字节索引该为字索引
+int u_char2bit(tuple_ a)
+{
+	return a.u_char_ * 8 + a.bit_;
+}
+//将字索引改为字节索引
+tuple_ bit2u_char(int a)
+{
+	tuple_ b;
+	b.u_char_ = a / 8;
+	b.bit_ = a % 8;
+	return b;
+}
+
+
+//取第几位
+int getbit(u_char a, int x)//从左至右第0-7位
+{
+	a = a << x;
+	a = a >> 7;
+	return a;
+}
+//在第几位写入
+void write(u_char& a, int x, int num)//将num写入a的第x位
+{
+	a += num << (7 - x);
+}
+
+//做对应转换
+void convert(u_char a[], u_char b[], int i, int conv[])//conv规则中的第i位完成转换写入到b中
+{
+	int index_bit_b = i;
+	tuple_ index_u_char_b = bit2u_char(index_bit_b);
+	int index_bit_a = conv[index_bit_b] - 1;
+	tuple_ index_u_char_a = bit2u_char(index_bit_a);
+	int temp = getbit(a[index_u_char_a.u_char_], index_u_char_a.bit_);
+	write(b[index_u_char_b.u_char_], index_u_char_b.bit_, temp);
+
+}
+
+
+
+//初始置换
+void convIP(u_char a[], u_char b[])//将a进行IP置换，结果存入b中
+{
+	for (int i = 0; i < 64; i++)
+	{
+		convert(a, b, i, IP);
+	}
+}
+//初始置换逆置换IP^(-1)
+void convIP_1(u_char a[], u_char b[])//逆初始置换，将a进行IP置换，结果存入b中
+{
+	for (int i = 0; i < 64; i++)
+		convert(a, b, i, IP_1);
+}
+
+
+
+
+
+//密钥产生
+void getkeys(u_char key[8], u_char key_final[16][6])
+{
+	//ofstream("keys.txt");
+	u_char keys[7] = {};
+	for (int i = 0; i < 56; i++)
+		convert(key, keys, i, PC_1);
+	bitset<56> key_round[17];
+	//初始化全为0
+	for (int i = 0; i < 16; i++)key_round[i].reset();
+	//信息复制
+	for (int i = 0; i < 7; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			if (getbit(keys[i], j) == 1)
+				key_round[0].set(i * 8 + j);
+		}
+	}
+	for (int i = 0; i < 16; i++)
+	{
+		bitset<56> left_mov = key_round[i] >> MOV[i];//注意大小端序问题，实际是左移
+		for (int j = 0; j < MOV[i]; j++)
+		{
+			left_mov.set(28 - MOV[i] + j, key_round[i][j]);
+			left_mov.set(56- MOV[i] + j, key_round[i][28 + j]);
+		}
+		key_round[i+1] = left_mov;
+	}
+
+	//PC_2置换
+	for(int i=0;i<16;i++)
+		for (int j = 0; j < 48; j++)
+		{
+			write(key_final[i][j/8], j%8, key_round[i + 1][PC_2[j] - 1]);
+		}
+}
+
+
+
+//轮加密
+void round(u_char m_[8], u_char key_final[16][6], u_char afterIP_1[8])
+{
+	u_char m[8] = {};
+	convIP(m_, m);
+	u_char left[4], right[4];
+	for (int i = 0; i < 4; i++)
+	{
+		left[i] = m[i];
+		right[i] = m[i+4];
+	}
+ 	for (int i = 0; i < 16; i++)//十六轮加密
+	{
+		////cout << i<<": left ";
+		//cout << "{";
+		//for (int i = 0; i < 4; i++)//cout << getbit(left[i/8], i%8);
+		//printf("0x%02x, ", left[i]);
+		////cout << "right ";
+		//for (int i = 0; i <4; i++)//cout << getbit(right[i / 8], i % 8);
+		//printf("0x%02x, ", right[i]);
+		//cout << "},"<<endl;
+
+		u_char afterE[6] = {};//扩展置换后的结果
+		//扩展置换E
+		convertE(right,afterE);
+		//与密钥进行逐bit异或
+		u_char afterXOR[6] = {};
+		xor_(key_final[i], afterE, afterXOR);
+		//S盒代换
+		u_char afterS[4] = {};
+		convertS(afterXOR, afterS);
+		//P置换
+		u_char afterP[4] = {};
+		convertP(afterS, afterP);
+		u_char afterXOR2[4] = {};
+		for (int i = 0; i < 4; i++)//逐比特异或
+		{
+			afterXOR2[i] = left[i] ^ afterP[i];
+		}
+		//生成下一轮的left和right
+		for (int i = 0; i < 4; i++)left[i] = right[i];
+		for (int i = 0; i < 4; i++)right[i] = afterXOR2[i];
+		int ix = 0;
+	}
+	////cout << "16: left ";
+	//cout << "{";
+	//for (int i = 0; i < 4; i++)printf("0x%02x, ", left[i]);
+	////cout << "right ";
+	//for (int i = 0; i < 4; i++)printf("0x%02x, ", right[i]);
+	//cout << "}," << endl;
+
+
+
+	//左右交换
+	{
+		u_char temp[4] = {};
+		for (int i = 0; i < 4; i++)
+		{
+			temp[i] = left[i];
+			left[i] = right[i];
+			right[i] = temp[i];
+		}
+	}
+	//合并
+	u_char afterCombine[8];
+	for (int i = 0; i < 4; i++)
+	{
+		afterCombine[i] = left[i];
+		afterCombine[i + 4] = right[i];
+	}
+	//逆初始置换
+	convIP_1(afterCombine, afterIP_1);
+}
+
+
+
+
+//扩展置换
+void convertE(u_char a[4], u_char b[6])
+{
+	for (int i = 0; i < 48; i++)
+		convert(a, b, i, E);
+}
+//48比特异或
+void xor_(u_char a[6], u_char b[6], u_char c[6])
+{
+	for(int i=0;i<6;i++)
+		for (int j = 0; j < 8; j++)
+		{
+			int temp = getbit(a[i], j) ^ getbit(b[i], j);
+			write(c[i], j, temp);
+		}
+}
+//S盒
+void convertS(u_char a[6],u_char b[4])
+{
+	bitset<48> a_;
+	u_char b_[8];
+	a_.reset();
+	//数据类型转换
+	for (int i = 0; i < 48; i++)
+	{
+		a_.set(i, getbit(a[i / 8], i % 8));
+	}
+	//S盒
+	for (int i = 0; i < 8; i++)
+	{
+		//行
+		int box_index = a_[i * 6] * 2 + a_[i * 6 + 5];
+		//列
+		int line_index = 0;
+		for (int j = 1; j <= 4; j++)
+		{
+			line_index = line_index * 2 + a_[i * 6 + j];
+		}
+		b_[i] = S[i][box_index*16+line_index];
+	}
+	for(int i=0;i<4;i++)
+		for (int j = 0; j < 4; j++)
+		{
+			write(b[i], j, getbit(b_[2*i],4+j));
+			write(b[i], j+4, getbit(b_[2*i+1],4+j));
+		}
+	int i = 0;
+}
+//P置换
+void convertP(u_char a[4], u_char b[4])
+{
+	for (int i = 0; i < 32; i++)
+		convert(a, b, i, P);
+}
+
+// int main()
+// {
+// 	//u_char a = 0;
+// 	//write(a, 1, 1);
+// 	//cout << "0x" <<hex<<(int)a<< endl;
+// //   a = 254;
+// 	//cout<<getbit(a, 1)<<endl;
+// 	//cout<<getbit(a, 8)<<endl;
+
+// 	/*u_char a[] = { 128,0,0,0,0,0,0,0,0 };
+// 	u_char b[8] = {};
+// 	u_char c[8] = {};
+// 	convIP(a, b);
+// 	convIP_1(b, c);*/
+
+// 	//u_char key[8] = { 0x10, 0x31, 0x6E, 0x02, 0x8C, 0x8F, 0x3B, 0x4A };
+// 	//u_char key_final[16][6];
+// 	//getkeys(key,key_final);
+
+// 	/*u_char a[] = { 0x10, 0x31, 0x6E, 0x02 };
+// 	u_char b[6] = {};
+// 	convertE(a, b);*/
+// 	u_char a[] = { 0x10, 0x31, 0x6E, 0x02, 0x8C, 0x8F };
+// 	u_char b[4] = {};
+// 	convertS(a, b);
+	
+// 	int i = 0;
+// }
